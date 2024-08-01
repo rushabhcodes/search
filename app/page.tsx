c18230f72920bc671ef8abd7daedcac91756320e
rushabhcodes <rushabhpatil557@gmail.com>
2024-08-01 12:48:29 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { GlobeDemo } from "@/components/search";
+import { Button } from "@/components/ui/button";
+import { Globe } from "@/components/ui/globe";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { FaSearch } from "react-icons/fa";
+
+
+
+export default function Home() {
+  return (
+   <>
+   <GlobeDemo/>
+ </>
+  );
+}
